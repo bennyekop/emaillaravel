dcd57c6a22e336c690fdcbd8904d83ea0a52ea43
bennyekop <bennyekop4@gmail.com>
2019-04-14 13:31:12 +0700

    commit pertama email smtp laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+@extends('base')
+@section('content')
+    <!-- Main Section -->
+    <section class="main-section">
+        <!-- Add Your Content Inside -->
+        <div class="content">
+            <!-- Remove This Before You Start -->
+            <h1>Rumahweb -  Tutorial Laravel </h1>
+            <p>Apa saja yang akan kamu pelajari ?</p>
+
+            <h2>* Templating Laravel </h2>
+            <h2>* CRUD dengan Laravel  (Soon)</h2>
+            <h2>* Upload Image dengan laravel  (Soon)</h2>
+            <h2>* Login & Register dengan Auth & Session built in Laravel  (Soon)</h2>
+            <h2>* Kirim E-mail dengan Laravel  (Soon)</h2>
+            <h2>* Basic Web Service dengan Laravel  (Soon)</h2>
+            <h2>* Laravel OAuth 2.0 dengan Laravel Passport  (Soon)</h2>
+            <h2>* Perkenalan dengan Lumen (Microframework Laravel untuk REST API) (Soon)</h2>
+        </div>
+        <!-- /.content -->
+    </section>
+    <!-- /.main-section -->
+@endsection
