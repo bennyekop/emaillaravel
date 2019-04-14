8e72e4d9c7788bf149499feb9f9a7f5a69d213b6
bennyekop <bennyekop4@gmail.com>
2019-04-14 20:27:38 +0700

    ini coba ssh key dan edit list template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,13 +9,7 @@
             <p>Apa saja yang akan kamu pelajari ?</p>
 
             <h2>* Templating Laravel </h2>
-            <h2>* CRUD dengan Laravel  (Soon)</h2>
-            <h2>* Upload Image dengan laravel  (Soon)</h2>
-            <h2>* Login & Register dengan Auth & Session built in Laravel  (Soon)</h2>
-            <h2>* Kirim E-mail dengan Laravel  (Soon)</h2>
-            <h2>* Basic Web Service dengan Laravel  (Soon)</h2>
-            <h2>* Laravel OAuth 2.0 dengan Laravel Passport  (Soon)</h2>
-            <h2>* Perkenalan dengan Lumen (Microframework Laravel untuk REST API) (Soon)</h2>
+            <h2>- Basic Web Service dengan Laravel </h2>
         </div>
         <!-- /.content -->
     </section>
